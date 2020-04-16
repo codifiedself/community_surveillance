e4ffe92eaeae7d6d63ba76c405b26b494c1cea2c
Nitin <nitin.pande@gmail.com>
2020-04-16 21:47:43 +0530

    changed the base url to become a language selection screen

--- webapp/urls.py
+++ webapp/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     url(r'^select2/', include('django_select2.urls')),
 
     path('ngo/create/', ngo_create, name='ngo_create'),
-    path('covid19-ngo-collaboration-form', ngo_create, name='ngo_create'),
+    path('covid19-ngo-collaboration-form', select_language_for_form, name='select_language_for_form'),
 
 
     path('ngo/details/', ngo_details, name='ngo_details'),
     path('ngo_district_list_and_form/', ngo_district_list_and_form, name='ngo_district_list_and_form'),
 
-    path('select_language_for_form/', select_language_for_form, name='select_language_for_form'),
+    # path('select_language_for_form/', select_language_for_form, name='select_language_for_form'),
 
 
     
